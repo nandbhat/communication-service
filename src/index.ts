26cf6d2cded0937272d3564a8104c040ed9a998e
nandbhat <nandanbhatd@gmail.com>
2023-09-10 19:46:45 +0530

    Add json response

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,9 @@ import { Hono } from 'hono'
 
 const app = new Hono()
 
-app.get('/', (c) => c.text('Hello Hono!'))
+app.get('/', (c) => c.json({
+    ok: true,
+    message: 'Hello Hono!',
+}))
 
 export default app
