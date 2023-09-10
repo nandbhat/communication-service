8f520dd297bddeffa296502c081610068605fc5d
nandbhat <nandanbhatd@gmail.com>
2023-09-10 19:16:55 +0530

    Initialise bun project

---
+++ src/index.test.ts
@@ -0,0 +1,12 @@
+// @ts-ignore
+import { describe, expect, it } from 'bun:test'
+import app from '.'
+
+describe('My first test', () => {
+    // @ts-ignore
+    it('Should return 200 Response', async () => {
+        const req = new Request('http://localhost/')
+        const res = await app.fetch(req)
+        expect(res.status).toBe(200)
+    })
+})
