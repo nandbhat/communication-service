ff19b88d71f2c9d55b0e2dada7f21d9478f97e8f
nandbhat <nandanbhatd@gmail.com>
2023-09-10 19:53:14 +0530

    Add info call

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,17 @@
 import { Hono } from 'hono'
+import pkg from '../package.json'
 
 const app = new Hono()
 
 app.get('/', (c) => c.json({
     ok: true,
     message: 'Hello Hono!',
+}))
+
+app.get('/info', (c) => c.json({
+    ok: true,
+    name: pkg.name,
+    version: pkg.version
 }))
 
 export default app
