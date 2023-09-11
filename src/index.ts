7f77fba4b8c62e841f5a41a8e5a074942499f392
nandbhat <nandanbhatd@gmail.com>
2023-09-11 17:40:18 +0530

    Refactor rabbit mq files

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,7 @@
 import { Hono } from 'hono'
 import pkg from '../package.json'
+import amqp from 'amqplib/callback_api'
+
 
 const app = new Hono()
 
@@ -13,5 +15,59 @@ app.get('/info', (c) => c.json({
     name: pkg.name,
     version: pkg.version
 }))
+
+
+amqp.connect('amqp://localhost', function(error0, connection) {
+    if (error0) {
+        throw error0;
+    }
+    connection.createChannel(function(error1, channel) {
+        if (error1) {
+            throw error1;
+        }
+
+        var queue = 'hello';
+        var msg = 'Hello World!';
+
+        channel.assertQueue(queue, {
+            durable: false
+        });
+        channel.sendToQueue(queue, Buffer.from(msg));
+
+        console.log(" [x] Sent %s", msg);
+    });
+    setTimeout(function() {
+        connection.close();
+        process.exit(0);
+    }, 500);
+});
+
+
+
+amqp.connect('amqp://localhost', function(error0, connection) {
+    if (error0) {
+        throw error0;
+    }
+    connection.createChannel(function(error1, channel) {
+        if (error1) {
+            throw error1;
+        }
+
+        var queue = 'hello';
+
+        channel.assertQueue(queue, {
+            durable: false
+        });
+
+        console.log(" [*] Waiting for messages in %s. To exit press CTRL+C", queue);
+
+        channel.consume(queue, function(msg) {
+            // @ts-ignore
+            console.log(" [x] Received %s", msg.content.toString());
+        }, {
+            noAck: true
+        });
+    });
+});
 
 export default app
